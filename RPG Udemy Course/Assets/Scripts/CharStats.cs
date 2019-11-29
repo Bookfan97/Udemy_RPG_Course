925805a35cdd6d7dd81eb38c815a7717baf80e3e
Noah <sheltielover432@gmail.com>
2019-11-28 21:19:05 -0500

    Levelling System

--- RPG Udemy Course/Assets/Scripts/CharStats.cs
+++ RPG Udemy Course/Assets/Scripts/CharStats.cs
@@ -7,7 +7,9 @@ public class CharStats : MonoBehaviour
     public string charName;
     public int playerLevel = 1;
     public int currentEXP;
-
+    public int[] expToNextLevel;
+    public int maxLevel = 100;
+    public int baseEXP = 1000;
     public int currentHP;
     public int maxHP = 100;
     public int currentMP;
@@ -23,12 +25,30 @@ public class CharStats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        expToNextLevel = new int[maxLevel];
+        expToNextLevel[1] = baseEXP;
+        for (int i=2; i<expToNextLevel.Length; i++)
+        {
+            expToNextLevel[i] = Mathf.FloorToInt(expToNextLevel[i-1]*1.05f);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.K))
+        {
+            AddExp(500);
+        }
+    }
+
+    public void AddExp(int expToAdd)
+    {
+        currentEXP += expToAdd;
+        if(currentEXP>expToNextLevel[playerLevel])
+        {
+            currentEXP -= expToNextLevel[playerLevel];
+            playerLevel++;
+        }
     }
 }
