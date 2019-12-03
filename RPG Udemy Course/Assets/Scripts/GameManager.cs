ffbf1668c0b667f2be28f7f780eda2b4b906eac8
Noah <sheltielover432@gmail.com>
2019-12-02 21:14:48 -0500

    Storing our Inventory and Getting Item Details

--- RPG Udemy Course/Assets/Scripts/GameManager.cs
+++ RPG Udemy Course/Assets/Scripts/GameManager.cs
@@ -9,6 +9,10 @@ public class GameManager : MonoBehaviour
     public bool gameMenuOpen;
     public bool dialogActive;
     public bool fadingBetweenAreas;
+    public string[] itemsHeld;
+    public int[] numberOfItems;
+    public Item[] referenceItems;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,5 +31,16 @@ public class GameManager : MonoBehaviour
         {
             PlayerController.instance.canMove = true;
         }
+    }
+    public Item GetItemDetails(string itemToGrab)
+    {
+        for (int i=0; i<itemsHeld.Length; i++)
+        {
+            if(referenceItems[i].itemName == itemToGrab)
+            {
+                return referenceItems[i];
+            }
+        }
+        return null;
     }
 }
