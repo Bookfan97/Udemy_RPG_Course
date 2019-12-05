ff57e3be99b07b3c265e326146eae4b12fd5840f
Noah <sheltielover432@gmail.com>
2019-12-04 21:53:21 -0500

    Picking up items

--- RPG Udemy Course/Assets/Scripts/GameManager.cs
+++ RPG Udemy Course/Assets/Scripts/GameManager.cs
@@ -18,6 +18,7 @@ public class GameManager : MonoBehaviour
     {
         instance = this;
         DontDestroyOnLoad(gameObject);
+        SortItems();
     }
 
     // Update is called once per frame
