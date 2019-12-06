480fc3a3210f6156b1490ca664d5475ba5ac73a8
Noah <sheltielover432@gmail.com>
2019-12-05 22:19:53 -0500

    Checking and marking quests

--- RPG Udemy Course/Assets/Scripts/QuestManager.cs
+++ RPG Udemy Course/Assets/Scripts/QuestManager.cs
@@ -19,4 +19,35 @@ public class QuestManager : MonoBehaviour
     {
         
     }
+
+    public int GetQuestNumber(string questToFind)
+    {
+        for (int i=0; i<questMarkerNames.Length; i++)
+        {
+            if(questMarkerNames[i] == questToFind)
+            {
+                return i;
+            }
+        }
+        Debug.LogError("Quest " + questToFind + " does not exist");
+        return 0;
+    }
+    public bool CheckIfComplete(string questToCheck)
+    {
+        if(GetQuestNumber(questToCheck) !=0)
+        {
+            return questMarkersComplete[GetQuestNumber(questToCheck)];
+        }
+        return false;
+    }
+
+    public void MarkQuestComplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = true;
+    }
+
+    public void MarkQuestIncomplete(string questToMark)
+    {
+        questMarkersComplete[GetQuestNumber(questToMark)] = false;
+    }
 }
